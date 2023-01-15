d76aeaf90b00bebe6bc04eb5c96560c4c436ddac
FkkfRf <Fkkf.Rf@gmail.com>
2023-01-15 19:19:07 +0300

    Init

---
+++ src/test/java/android/WikipediaTest.java
@@ -0,0 +1,22 @@
+package android;
+
+import io.appium.java_client.AppiumBy;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
+
+public class WikipediaTest {
+    @Test
+    void successSearchTest() {
+        back();
+        step("Type search", () -> {
+            $(AppiumBy.id("org.wikipedia.alpha:id/search_container")).click();
+            $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Java");
+        });
+        step("Verify content found", () ->
+                $$(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title"))
+                        .shouldHave(sizeGreaterThan(0)));
+    }
+}
